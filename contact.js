1ced983533a8498519d6f3af28aee4cd4a0c36da
Gugan_ay <gugansankar07@gmail.com>
2026-06-19 17:39:45 +0530

    3rd commit

---
+++ contact.js
@@ -0,0 +1,18 @@
+import Home from "./Home";
+import Contact from "./Contact";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <h2>My React Website</h2>
+      </nav>
+
+      <Home />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
---
+++ static/js/488.ab880208.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[488],{488(){new WeakMap;new Set}}]);
+//# sourceMappingURL=488.ab880208.chunk.js.map
